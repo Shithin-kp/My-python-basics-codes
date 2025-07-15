ef502e732aa85b9e0165169d4ed21895b119bf79
Shithin-kp <shithinkp1999@gmail.com>
2025-07-15 09:39:03 +0530

    python basics codes

---
+++ python/casting.py
@@ -0,0 +1,5 @@
+for i in range(5):  # Iterates from 0 to 4
+    print(i)
+name = "Python"
+for char in name:
+    print(char)
---
+++ python/ifelse.py
@@ -0,0 +1,5 @@
+Meghna=input("Meghna died or not?")
+if(Meghna=="Died"):
+    print("Suriya meets priya")
+else:
+    print("Suriya weds meghna")
